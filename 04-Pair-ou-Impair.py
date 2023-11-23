0c677199c2f147303ac8e75c2fa9b4de594687cc
eltax42 <jquidu@yahoo.com>
2023-11-23 01:12:24 +0100

    23 nov save

--- 04-Pair-ou-Impair.py
+++ 04-Pair-ou-Impair.py
@@ -1,14 +1,13 @@
-def pair_impair(a,b):
-    try:
-        a = int(a)
-        if a%b == 0:
-            print("Votre chiffre est Pair")
-        else:
-            print("Votre chiffre est Impair")
-    except:
-        print("Vous devez entrer un Nombre !")
-
 a = input("Ecrivez un nombre: ")
 b = 2
+try:
+    a = int(a)
+    if a%b == 0:
+        print("Votre chiffre est Pair")
+    else:
+        print("Votre chiffre est Impair")
+except:
+    print("Vous devez entrer un Nombre !")
 
-pair_impair(a,b)
+
+
