94c909276c46ca8ef2c4ef443982f54ed510dea9
eltax42 <jquidu@yahoo.com>
2023-11-30 10:45:21 +0100

    30 nov save
    final version

--- 04_Pair_ou_Impair.py
+++ 04_Pair_ou_Impair.py
@@ -1,14 +1,26 @@
 def calcul():
-    a = input("Ecrivez un nombre: ")
     b = 2
     
     if a%b == 0:
-        print("Votre chiffre est Pair")   
+        print("Votre chiffre est Pair .")   
     else:
-        print("Votre chiffre est Impair")    
+        print("Votre chiffre est Impair .")    
+    
+print("------> Debut du programme <------", ('\n'))
+a = input("Ecrivez un nombre entier : ")
+
+try:
+    a = int(a)
+    calcul()
+except:
+    print("Erreur ! Entrez un nombre entier !")
+
+print(("\n"), "------> fin du programe <------")
 
 
-calcul()
+
+
+
 
         
 
