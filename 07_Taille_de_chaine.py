45ee1832eb9a166f81d0174d0c4e6beb65cd3213
eltax42 <jquidu@yahoo.com>
2023-12-02 05:10:05 +0100

    save
    pas sur d'avoir compris l'exercice

--- 07_Taille_de_chaine.py
+++ 07_Taille_de_chaine.py
@@ -1,9 +1,12 @@
-#1.0
-Message = input("Ecrivez un message --> ")
-n_lettres = 0
-
-for i in Message:
+def t_chaine(a):
+    n_lettres = 0
+    for lettre in a:   
         n_lettres += 1
+    print("La taille de la chaine est --> ",n_lettres)
 
-print(n_lettres)
+print("------> Debut du programme <------", ('\n'))
 
+Message = input("Ecrivez un message --> ")
+t_chaine(Message)
+
+print(("\n"), "------> fin du programe <------")
